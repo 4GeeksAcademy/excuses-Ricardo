d856f7163f0b4d44b69dafa52a9a52dffea66c2c
Ricardo <rick_fm_95@hotmail.com>
2025-06-03 02:52:19 +0000

    Updated

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload  = () =>{
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+    const pickRandomElement = (list) =>list[
+        Math.floor(Math.random()*list.length)];
+    
+
+    let excusa = `${pickRandomElement(who)} ${pickRandomElement(action)} ${pickRandomElement(what)} ${pickRandomElement(when)}`;
+    let doc = document.getElementById("excuse")
+    doc.textContent = excusa;
+
+}
+
+
